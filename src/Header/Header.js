aa18c9d22e0b965f8e0f094d5b6bd758d4e8ec64
Khursheed Gaddi <khursheed.gaddi@e-zest.in>
2022-08-22 11:42:31 +0530

    Test App Base

---
+++ src/Header/Header.js
@@ -0,0 +1,7 @@
+import React  from "react";
+import classes from './Header.css'
+const pereson = ()=>{
+    return <h1 className={classes.Header}>Welcome to First App</h1>
+}
+
+export default pereson;
